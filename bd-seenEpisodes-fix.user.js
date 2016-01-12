4397fc8c0e2c2520ba11a9669acb8351c95ead40
Robin Houtevelts <robin@houtevelts.com>
2016-01-12 18:25:52 +0100

    Run at document-start

--- bd-seenEpisodes-fix.user.js
+++ bd-seenEpisodes-fix.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name       Bierdopje seen episodes fix
 // @namespace  http://www.bierdopje.com
-// @version    0.01
+// @version    0.02
 // @description  Hides seen episodes at the unseen episodes-overview.
 // @include    /^http://www\.bierdopje\.com/users?/.*?/shows/episodes/*/
 // @require    http://code.jquery.com/jquery-2.1.4.min.js
 // @copyright  2016+, Robin Houtevelts
+// @run-at     document-start
 // ==/UserScript==
 
 if (window.top != window.self)
